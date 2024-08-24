8c7df6db07b22096f7f1332d7b700ce0b0b0290b
Yash <147479013+Yash-978@users.noreply.github.com>
2024-08-24 23:56:26 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -85,22 +85,7 @@ public class Main {
 
     }
 
-            /*static int sumOfPrimes(int n) {
-                boolean prime[] = new boolean[n + 1];
-                Arrays.fill(prime, true);
-                for (int p = 2; p * p <= n; p++) {
-                    if (prime[p] == true) {
-                        for (int i = p * 2; i <= n; i += p) prime[i] = false;
-                    }
-                }
-                int sum = 0;
-                for (int i = 2; i <= n; i++) if (prime[i]) sum += i;
-                return sum;
-            }
-
-            int n = 24;
-            System.out.print(sumOfPrimes(n));*/
-
+    
 
 
 
