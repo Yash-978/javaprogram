14ec862384eeecbecad3c4c59f390ca729da21b8
Yash <147479013+Yash-978@users.noreply.github.com>
2024-08-24 23:57:00 +0530

    Update primeNumber.java

--- src/primeNumber.java
+++ src/primeNumber.java
@@ -1,34 +1,4 @@
-//public class primeNumber {
-//
-//    //WAP TO SORT ARRAY IN ASCENDING(LOW TO HIGH) ORDER IN JAVA.
-//    int []array1 = new int[]{30,20,10,60,70,69,62};
-//    int temp=0;
-//        System.out.println("");
-//
-//        for (int i= 0;i<array1.length;i++)
-//    {
-//        System.out.println(array1[i] + "");
-//
-//    }
-//        for (int i=0;i<array1.length;i++)
-//    {
-//        for(int j=i+1;j<array1.length;j++)
-//        {
-//            if(array1[i]>array1[j])
-//            {
-//                temp=array1[i];
-//                array1[i]=array1[j];
-//                array1[j]=temp;
-//
-//            }
-//        }
-//    }
-//        System.out.println("Ascending order : ");
-//        for (int i=0;i<array1.length;i++)
-//    {
-//        System.out.println(array1[i]+ " ");
-//    }
-//}
+
 import java.util.Arrays;
 
 class primeNumber {
